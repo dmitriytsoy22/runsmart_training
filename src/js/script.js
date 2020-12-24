237a8c0a0327f20f843e4c31e6b3bccbd3ef8c94
Dmitriy <dimon_op@mail.ru>
2020-12-24 19:17:55 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,5 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-              }
-            }
-          ]
-      });
+    $('.surprice__button').on('click', function(){
+        $('.overlay, .ticked__img').fadeIn();
+    });
 });
